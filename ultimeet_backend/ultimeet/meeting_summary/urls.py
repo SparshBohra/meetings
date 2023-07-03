51d433ce828a81fb9ed717184eae3df5a60788d2
Sparsh Bohra <abc@example.com>
2023-07-03 16:11:52 +0800

    add changes

--- ultimeet_backend/ultimeet/meeting_summary/urls.py
+++ ultimeet_backend/ultimeet/meeting_summary/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
    path('meeting/<int:meeting_id>/summary_view/', views.summary_view, name='summary_view'),
    path('meeting/<int:meeting_id>/agenda/', views.agenda, name='agenda'),
    path('meeting/<int:meeting_id>/key_points/', views.key_points, name='key_points'),
+   path('meeting/<int:meeting_id>/audio_labels/', views.audio_labels, name='audio_labels'),
+   path('meeting/<int:meeting_id>/users_audio_breakpoints/', views.users_audio_breakpoints, name='users_audio_breakpoints'),
 ]
 
