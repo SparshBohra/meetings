e0e805cdbab622fdbe8a4c4175a08eb5a63193cd
Sparsh Bohra <abc@example.com>
2023-07-03 14:22:04 +0800

    add changes

--- ultimeet_backend/ultimeet/ultimeet/urls.py
+++ ultimeet_backend/ultimeet/ultimeet/urls.py
@@ -1,6 +1,7 @@
 
 #from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -8,6 +9,11 @@ urlpatterns = [
     path('recording_transcription/', include('recording_transcription.urls')),
     path('meeting_summary/', include('meeting_summary.urls')),
     path('meeting_action_tasks/', include('meeting_action_tasks.urls')),
+
+   # path('', views.home, name='home'),
+    #path('start_recording/', views.start_recording, name='start_recording'),
+    #path('stop_recording/', views.stop_recording, name='stop_recording'),
+    #path('process_speech/', views.process_speech, name='process_speech'),
 
 ]
 
